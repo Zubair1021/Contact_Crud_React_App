c8d37390c273fb97d3e87192ddba6fb902c76e38
MuhammadZubair <Zubairjaved1014@gmail.com>
2024-09-13 15:14:59 +0500

    Completed All Done

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div style={{marginTop:"150px"}}>
+      <h2>
+        {" "}
+        This is the React Firebase CRUD Application. This application is used to Managing Contact more efficiently.
+      </h2>
+      <h4 style={{marginTop:"70px", color:"#009879"}}>
+      &copy; 2024 ZJ Developer. All rights reserved.
+      </h4>
+    </div>
   )
 }
 
--- src/pages/View.js
+++ src/pages/View.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import fireDb from "../firebase";
+import { Link, useParams } from "react-router-dom";
+import { ref, get } from "firebase/database";
+import "./View.css";
 
 const View = () => {
-  return (
-    <div>View</div>
-  )
-}
+  const [user, setuser] = useState({});
+  // Grab Id from URL
+  const { id } = useParams();
 
-export default View
+  useEffect(
+    () => {
+      const contactsRef = ref(fireDb, `contacts/${id}`);
+
+      get(contactsRef).then((snapshot) => {
+        if (snapshot.exists()) {
+          setuser({ ...snapshot.val() });
+        } else {
+          setuser({});
+        }
+      });
+    },
+    { id }
+  );
+  console.log(user);
+  return (
+    <div style={{ marginTop: "150px" }}>
+      <div className="card">
+        <div className="card-header">
+          <p>User Contact Detail</p>
+          </div>
+          <div className="container">
+            <strong>
+              ID:
+              <span>{id}</span>
+            </strong>
+            <br />
+            <br />
+            <strong>
+              Name:
+              <span>{user.name}</span>
+            </strong>
+            <br />
+            <br />
+            <strong>
+              Email:
+              <span>{user.email}</span>
+            </strong>
+            <br />
+            <br />
+            <strong>
+              Contact:
+              <span>{user.contact}</span>
+            </strong>
+            <br />
+            <br />
+            <Link to='/'>
+            <button className="btn btn-edit">Go Back</button>
+            </Link>
+          </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default View;
